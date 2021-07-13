3940dfc1dbfc8c59652eecee8055404e0c1f995c
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-07-13 18:17:33 -0400

    add ranime & details

--- bot.py
+++ bot.py
@@ -1,23 +1,37 @@
 import os
+import random
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
-intents = discord.Intents(members=True)
-client = discord.Client(intents=intents)
+from main_webscrape import *
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+# intents = discord.Intents(members=True, messages=True, guilds=True)
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='?', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'{client.user.name} has connected to Discord!')
+    print(f'{bot.user.name} has connected to Discord!')
 
-@client.event
+@bot.event
 async def on_member_join(member):
-    print(member.name)
-    await member.send(f'Welcome {member.name} to the server!')
+    print(f"{member.name} has joined.")
+    await member.send(f'Welcome {member.name} to the server!\nUse ?help for more info!')
 
-client.run(TOKEN)
+@bot.command(name="ranime", help="Recommends a random show.")
+async def ranime(ctx):
+    reply = random.choice(list(name_id.keys()))
+    await ctx.send(reply)
+
+@bot.command(name="details", help="Display show stats on myanimelist.")
+async def details(ctx, *show: str):
+    show_name = ''.join(show)
+    reply = get_anime_stats(show_name)
+    await ctx.send(reply)
+
+bot.run(TOKEN)
