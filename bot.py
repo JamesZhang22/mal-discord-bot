cd0e94d4169c327c5806db5ad0cbe0aa7df7cc26
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-07-14 15:04:59 -0400

    start embeds

--- bot.py
+++ bot.py
@@ -10,7 +10,6 @@ from main_webscrape import *
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-# intents = discord.Intents(members=True, messages=True, guilds=True)
 intents = discord.Intents().all()
 activity = discord.Game(name="?help")
 bot = commands.Bot(command_prefix='?', intents=intents, activity=activity)
@@ -23,6 +22,19 @@ async def on_ready():
 async def on_member_join(member):
     print(f"{member.name} has joined.")
     await member.send(f'Welcome {member.name} to the server!\nUse ?help for more info!')
+
+@bot.command()
+async def displayembed(ctx):
+    embed = discord.Embed(title="Title", description="Test", color=discord.Color.blue())
+    embed.set_footer(text="footer")
+    embed.set_image(url="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg")
+    embed.set_thumbnail(url="https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg")
+    embed.set_author(name="Bot", icon_url="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg")
+    embed.add_field(name="Field", value="val", inline=False)
+    embed.add_field(name="Field", value="val", inline=True)
+    embed.add_field(name="Field", value="val", inline=True)
+
+    await ctx.send(embed=embed)
 
 @bot.command(help="Get bot's latency in milliseconds.")
 async def ping(ctx):
@@ -42,11 +54,16 @@ async def watch(ctx, *question):
 async def ranime(ctx, genre=None):
     reply = random.choice(list(name_id.keys()))
     if not genre:
-        await ctx.send(f"{reply}: https://myanimelist.net/anime/{name_id[reply]}")
+        await ctx.send(f"https://myanimelist.net/anime/{name_id[reply]}")
     else:
         if genre.lower() in all_genres:
             reply = random.choice(get_genre_list(genre.lower())).replace("!", "")
-            await ctx.send(f"Genre: {genre.capitalize()}\nAnime: {reply}\nLink: https://myanimelist.net/anime/{name_id[reply.lower()]}")
+            embed = discord.Embed(title=f"Anime: {reply.capitalize()}", color=discord.Color.blue())
+            embed.set_author(name=f"Genre: {genre.capitalize()}")
+            embed.set_thumbnail(url="https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg")
+            embed.add_field(name="Link", value=f"https://myanimelist.net/anime/{name_id[reply.lower()]}", inline=False)
+
+            await ctx.send(embed=embed)
         else:
             await ctx.send("Not a valid genre!")
 
@@ -57,5 +74,8 @@ async def details(ctx, *show: str):
     print(show_name)
     reply = get_anime_stats(show_name)
     await ctx.send(reply)
+
+
+
 
 bot.run(TOKEN)
