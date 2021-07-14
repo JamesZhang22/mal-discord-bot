459adb641c5ef042e32ad5c08afc21cf9fd9f9db
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-07-14 12:04:23 -0400

    add get anime with same genre func

--- main_webscrape.py
+++ main_webscrape.py
@@ -1,6 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-from typing import Dict
+from typing import Dict, List
 
 
 def create_dict_of_name_id() -> Dict[int, str]:
@@ -18,7 +18,7 @@ def create_dict_of_name_id() -> Dict[int, str]:
 
 name_id = create_dict_of_name_id()
 
-all_genres = ['action', 'adventure', 'cars', 'comedy', 'dementia', 'demons', 'drama', 'ecchi', 'fantasy', 'game', 'harem', 'hentai', 'historical', 'horror', 'josei', 'kids', 'magic', 'martial arts', 'mecha', ' military', 'music', 'mystery', 'parody', 'police', 'psychological', 'romance', 'samurai', 'school', 'sci-fi', 'seinen', 'shoujo', 'shoujo ai', 'shounen', 'shounen ai', 'slice of life', 'space', 'sports', 'super power', 'supernatural', 'thriller', 'vampire', 'yoai', 'yuri']
+all_genres = ['action', 'adventure', 'cars', 'comedy', 'dementia', 'demons', 'drama', 'ecchi', 'fantasy', 'game', 'harem', 'hentai', 'historical', 'horror', 'josei', 'kids', 'magic', 'martial arts', 'mecha', 'military', 'music', 'mystery', 'parody', 'police', 'psychological', 'romance', 'samurai', 'school', 'sci-fi', 'seinen', 'shoujo', 'shoujo ai', 'shounen', 'shounen ai', 'slice of life', 'space', 'sports', 'super power', 'supernatural', 'thriller', 'vampire', 'yoai', 'yuri']
 
 
 def get_anime_stats(name: str) -> str:
@@ -51,6 +51,63 @@ def get_anime_stats(name: str) -> str:
     return name, rating, rank, popularity, episodes, genres, similars
 
 
-# example = get_anime_stats('Naruto')
-# print(example)
-# print(name_id)
+def get_genre_list(genre: str) -> List[str]:
+    shows = []
+    genres = {
+        'action': 1, 
+        'adventure': 2, 
+        'cars': 3, 
+        'comedy': 4, 
+        'dementia': 5, 
+        'demons': 6, 
+        'drama': 8, 
+        'ecchi': 9, 
+        'fantasy': 10, 
+        'game': 11, 
+        'harem': 35, 
+        'hentai': 12, 
+        'historical': 13, 
+        'horror': 14, 
+        'josei': 43, 
+        'kids': 15, 
+        'magic': 16, 
+        'martial arts': 17, 
+        'mecha': 18, 
+        'military': 38, 
+        'music': 19, 
+        'mystery': 7, 
+        'parody': 20, 
+        'police': 39, 
+        'psychological': 40, 
+        'romance': 22, 
+        'samurai': 21, 
+        'school': 23, 
+        'sci-fi': 24, 
+        'seinen': 42, 
+        'shoujo': 25, 
+        'shoujo ai': 26, 
+        'shounen': 27, 
+        'shounen ai': 28, 
+        'slice of life': 36, 
+        'space': 29, 
+        'sports': 30, 
+        'super power': 31, 
+        'supernatural': 37, 
+        'thriller': 41,
+        'vampire': 32, 
+        'yoai': 33, 
+        'yuri': 34
+    }
+
+    number = genres[genre.lower()]
+    url = "https://myanimelist.net/anime/genre/" + str(number)
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find(id="content")
+
+    name_wrapper = results.find_all("h2", class_="h2_anime_title")
+    for name in name_wrapper:
+        link = name.find("a")
+        shows.append(link.text)
+
+    return shows
