b6e2f2a7c9da54cbdd856ff5d9dfbf76f5d72431
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-07-15 11:13:22 -0400

    minor fix

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents().all()
 activity = discord.Game(name="?help")
-bot = commands.Bot(command_prefix='?', intents=intents, activity=activity)
+bot = commands.Bot(command_prefix='?', intents=intents, activity=activity, help_command=None)
 
 @bot.event
 async def on_ready():
@@ -23,19 +23,8 @@ async def on_member_join(member):
     print(f"{member.name} has joined.")
     await member.send(f'Welcome {member.name} to the server!\nUse ?help for more info!')
 
-@bot.command()
-async def displayembed(ctx):
-    embed = discord.Embed(title="Title", description="Test", color=discord.Color.blue())
-    embed.set_footer(text="footer")
-    embed.set_image(url="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg")
-    embed.set_thumbnail(url="https://i.pinimg.com/originals/5b/b4/8b/5bb48b07fa6e3840bb3afa2bc821b882.jpg")
-    embed.set_author(name="Bot", icon_url="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg")
-    embed.add_field(name="Field", value="val", inline=False)
-    embed.add_field(name="Field", value="val", inline=True)
-    embed.add_field(name="Field", value="val", inline=True)
 
-    await ctx.send(embed=embed)
-
+# Basic Commands
 @bot.command(help="Get bot's latency in milliseconds.")
 async def ping(ctx):
     await ctx.send(f"Pong!\n{round(bot.latency*1000)}ms")
@@ -45,6 +34,11 @@ async def ping(ctx):
 async def clear(ctx, amt=5):
     await ctx.channel.purge(limit=amt)
 
+@bot.command()
+async def help(ctx):
+    await ctx.send("Helping!")
+
+# Anime Commands
 @bot.command(help="Anime show 8 ball.")
 async def watch(ctx, *question):
     responses = ['Yes', 'Definetly', 'No', 'NO!', 'Perhaps', 'Go for it!', 'Nah', 'In a week', 'NEVER!', 'WATCH IT RIGHT NOW!']
@@ -52,13 +46,16 @@ async def watch(ctx, *question):
 
 @bot.command(help="Recommends a random show.")
 async def ranime(ctx, genre=None):
-    reply = random.choice(list(name_id.keys()))
+    reply = random.choice(list(name_id.keys())).replace("!", "").replace("/", '').replace("(", '').replace(")", '')
     if not genre:
-        await ctx.send(f"https://myanimelist.net/anime/{name_id[reply]}")
+        embed = discord.Embed(title=f"Anime: {get_anime_stats(reply)[0]}", color=discord.Color.blue())
+        embed.set_thumbnail(url=get_anime_stats(reply)[7])
+        embed.add_field(name="Link", value=f"https://myanimelist.net/anime/{name_id[reply]}", inline=False)
+        await ctx.send(embed=embed)
     else:
         if genre.lower() in all_genres:
-            reply = random.choice(get_genre_list(genre.lower())).replace("!", "")
-            embed = discord.Embed(title=f"Anime: {reply.capitalize()}", color=discord.Color.blue())
+            reply = random.choice(get_genre_list(genre.lower())).replace("!", "").replace("/", '').replace("(", '').replace(")", '')
+            embed = discord.Embed(title=f"Anime: {get_anime_stats(reply)[0]}", color=discord.Color.blue())
             embed.set_author(name=f"Genre: {genre.capitalize()}")
             embed.set_thumbnail(url=get_anime_stats(reply.lower())[7])
             embed.add_field(name="Link", value=f"https://myanimelist.net/anime/{name_id[reply.lower()]}", inline=False)
@@ -70,7 +67,7 @@ async def ranime(ctx, genre=None):
 @bot.command(help="Display show stats on myanimelist.")
 async def details(ctx, *show: str):
     show = str(show)
-    show_name = show.replace(', ', ' ').replace("'", '').replace('(', '').replace(')', '').lower()
+    show_name = show.replace(', ', ' ').replace("'", '').replace('(', '').replace(')', '').replace(",", '').lower()
     reply = get_anime_stats(show_name)
     await ctx.send(reply)
 
