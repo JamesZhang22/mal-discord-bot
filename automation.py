8df740403ac1db0edf1a19a062c1a71e506e0304
JamesZhang22 <jameszhangjunjie@gmail.com>
2021-07-24 18:45:47 -0400

    add img getter

--- automation.py
+++ automation.py
@@ -12,7 +12,7 @@ def get_top_result(name: str):
     driver = webdriver.Chrome(PATH)
 
     driver.get("https://myanimelist.net/anime.php")
-    print(driver.title)
+    # print(driver.title)
 
     search_bar = driver.find_element_by_id("q")
     search_bar.send_keys(name)
@@ -25,6 +25,31 @@ def get_top_result(name: str):
         soup = BeautifulSoup(page.content, "html.parser")
         results = soup.find(id="content")
         link = results.find("a", class_="hoverinfo_trigger")["href"]
+        return link
+
+    except:
+        driver.quit()
+
+    driver.close()
+
+
+def get_image_character(name: str) -> str:
+    PATH = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(PATH)
+
+    driver.get("https://myanimelist.net/character.php")
+
+    search_bar = driver.find_element_by_id("q")
+    search_bar.send_keys(name)
+    search_bar.send_keys(Keys.RETURN)
+
+    try:
+        content = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "content")))
+        cur_url = driver.current_url
+        page = requests.get(cur_url)
+        soup = BeautifulSoup(page.content, "html.parser")
+        results = soup.find(id="content")
+        link = results.find("img", class_="lazyload")["data-src"]
         return link
 
     except:
